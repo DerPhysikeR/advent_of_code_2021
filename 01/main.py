bc8b02e4d1f09551f3ffe26f1ef8eda5381c2a6d
paul.reiter <paul.reiter@thesiestagroup.com>
2021-12-01 18:35:44 +0100

    Add solution for day 1 part 2

--- 01/main.py
+++ 01/main.py
@@ -13,6 +13,24 @@ def count_depth_increases(depth_measurements):
     return depth_increases_counter
 
 
+def sum_sliding_window(values, window_length):
+    sums = []
+    for i, value in enumerate(values):
+        try:
+            sums.append(sum(values[i:i+3]))
+        except IndexError:
+            pass
+    return sums
+
+
+def count_windowed_depth_increases(depth_measurements, window_length=3):
+    sliding_window_sums = sum_sliding_window(
+        depth_measurements, window_length=window_length
+    )
+    return count_depth_increases(sliding_window_sums)
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input("input.txt")
     print(count_depth_increases(puzzle_input))
+    print(count_windowed_depth_increases(puzzle_input))
--- 01/test_main.py
+++ 01/test_main.py
@@ -1,5 +1,13 @@
-from main import read_puzzle_input, count_depth_increases
+from main import (
+    read_puzzle_input,
+    count_depth_increases,
+    count_windowed_depth_increases,
+)
 
 
 def test_count_depth_increases_with_test_input():
     assert 7 == count_depth_increases(read_puzzle_input("test_input.txt"))
+
+
+def test_count_windowed_depth_increases_with_test_input():
+    assert 5 == count_windowed_depth_increases(read_puzzle_input("test_input.txt"), 3)
