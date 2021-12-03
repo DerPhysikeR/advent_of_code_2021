0a4526f937b11955edc0699b65ab56e35273b0bc
paul.reiter <paul.reiter@thesiestagroup.com>
2021-12-03 08:49:40 +0100

    Add solution for day 3 part 2

--- 03/main.py
+++ 03/main.py
@@ -3,25 +3,25 @@ def read_puzzle_input(filename):
         return stream.read().strip().split("\n")
 
 
-def find_most_commnon_bits(binary_number_strings):
+def find_most_common_bit(bits, fill_value=None):
+    assert set(bits) == set(["0", "1"])
+    one_count = bits.count("1")
+    zero_count = bits.count("0")
+    if one_count > zero_count:
+        return "1"
+    elif one_count < zero_count:
+        return "0"
+    else:
+        if fill_value is None:
+            raise ValueError("The number of zeros and ones is equal")
+        else:
+            return fill_value
+
+
+def find_most_commnon_bits(binary_number_strings, fill_value=None):
     digits = []
     for idx in range(len(binary_number_strings[0])):
-        one_count = 0
-        zero_count = 0
-        for number in binary_number_strings:
-            digit = number[idx]
-            if digit == "1":
-                one_count += 1
-            elif digit == "0":
-                zero_count += 1
-            else:
-                ValueError("{digit=} is not a binary digit")
-        if one_count > zero_count:
-            digits.append("1")
-        elif one_count < zero_count:
-            digits.append("0")
-        else:
-            ValueError("The number of zeros and ones is equal")
+        digits.append(find_most_common_bit([n[idx] for n in binary_number_strings]))
     return "".join(digits)
 
 
@@ -37,7 +37,40 @@ def calc_gamma_and_epsilon_rate(puzzle_input):
     return gamma_rate, epsilon_rate
 
 
+def filter_numbers(puzzle_input, filter_function):
+    numbers = puzzle_input[:]
+    for idx in range(len(numbers[0])):
+        numbers = filter_function(numbers, idx)
+        if len(numbers) == 1:
+            break
+    else:
+        raise ValueError("There is never a single number left ...")
+    return decimal_from_binary_string(numbers[0])
+
+
+def ogr_filter_function(numbers, idx):
+    mcb = find_most_common_bit([n[idx] for n in numbers], "1")
+    return [n for n in numbers if n[idx] == mcb]
+
+
+def calc_oxygen_generator_rating(puzzle_input):
+    return filter_numbers(puzzle_input, ogr_filter_function)
+
+
+def csr_filter_function(numbers, idx):
+    mcb = find_most_common_bit([n[idx] for n in numbers], "1")
+    return [n for n in numbers if n[idx] != mcb]
+
+
+def calc_co2_scrubber_rating(puzzle_input):
+    return filter_numbers(puzzle_input, csr_filter_function)
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input("input.txt")
     gamma_rate, epsilon_rate = calc_gamma_and_epsilon_rate(puzzle_input)
     print(gamma_rate * epsilon_rate)
+
+    oxygen_generator_rating = calc_oxygen_generator_rating(puzzle_input)
+    co2_scrubber_rating = calc_co2_scrubber_rating(puzzle_input)
+    print(oxygen_generator_rating * co2_scrubber_rating)
--- 03/test_main.py
+++ 03/test_main.py
@@ -2,6 +2,8 @@ from main import (
     read_puzzle_input,
     find_most_commnon_bits,
     calc_gamma_and_epsilon_rate,
+    calc_oxygen_generator_rating,
+    calc_co2_scrubber_rating,
 )
 
 
@@ -13,3 +15,13 @@ def test_find_most_common_bits():
 def test_calc_gamma_rate():
     puzzle_input = read_puzzle_input("test_input.txt")
     assert (22, 9) == calc_gamma_and_epsilon_rate(puzzle_input)
+
+
+def test_calc_oxygen_generator_rating():
+    puzzle_input = read_puzzle_input("test_input.txt")
+    assert 23 == calc_oxygen_generator_rating(puzzle_input)
+
+
+def test_calc_co2_scrubber_rating():
+    puzzle_input = read_puzzle_input("test_input.txt")
+    assert 10 == calc_co2_scrubber_rating(puzzle_input)
