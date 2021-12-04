c74b40f4f90dbcd9864a1677ef229837e71f0bb5
Paul Reiter <reiter.paul@gmail.com>
2021-12-04 07:32:30 +0100

    Add solution for day 4 part 1

---
+++ 04/main.py
@@ -0,0 +1,52 @@
+class BingoBoard:
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self._number_set = set().union(*self.numbers)
+        self._hit_numbers = set()
+        self._row_sets = [set(row) for row in self.numbers]
+        self._col_sets = [set(col) for col in list(map(list, zip(*self.numbers)))]
+
+    def __getitem__(self, key):
+        return self.numbers[key]
+
+    def _check_if_won(self):
+        for rs in self._row_sets:
+            if rs.issubset(self._hit_numbers):
+                return True
+        for cs in self._col_sets:
+            if cs.issubset(self._hit_numbers):
+                return True
+        return False
+
+    def call_number(self, number):
+        if number in self._number_set:
+            self._hit_numbers.add(number)
+        return self._check_if_won()
+
+    def __repr__(self):
+        return f"BingoBoard({self.numbers})"
+
+    def score(self):
+        return sum(self._number_set.difference(self._hit_numbers))
+
+
+def read_puzzle_input(filename):
+    with open(filename) as stream:
+        called_numbers = [int(n) for n in stream.readline().strip().split(",")]
+        bingo_board_strings = stream.read().strip().split("\n\n")
+        bingo_board_numbers = [
+            [[int(n) for n in row.split()] for row in bbs.split("\n")]
+            for bbs in bingo_board_strings
+        ]
+    return called_numbers, [BingoBoard(bb) for bb in bingo_board_numbers]
+
+
+def find_first_winning_board_score(numbers_to_call, bingo_boards):
+    for number in numbers_to_call:
+        for i, bingo_board in enumerate(bingo_boards):
+            if bingo_board.call_number(number):
+                return number * bingo_board.score()
+
+
+if __name__ == "__main__":
+    print(find_first_winning_board_score(*read_puzzle_input("input.txt")))
---
+++ 04/test_main.py
@@ -0,0 +1,29 @@
+from main import (
+    BingoBoard,
+    read_puzzle_input,
+    find_first_winning_board_score,
+)
+
+
+def test_bingo_board_wins_by_row():
+    bb = BingoBoard([[1, 2], [3, 4]])
+    assert bb.call_number(1) == False
+    assert bb.call_number(2) == True
+
+
+def test_bingo_board_wins_by_col():
+    bb = BingoBoard([[1, 2], [3, 4]])
+    assert bb.call_number(1) == False
+    assert bb.call_number(3) == True
+
+
+def test_read_puzzle_input():
+    called_numbers, boards = read_puzzle_input("test_input.txt")
+    assert called_numbers[0] == 7
+    assert called_numbers[-1] == 1
+    assert boards[0][0][0] == 22
+    assert boards[-1][-1][-1] == 7
+
+
+def test_find_first_winning_board_score():
+    assert 4512 == find_first_winning_board_score(*read_puzzle_input("test_input.txt"))
