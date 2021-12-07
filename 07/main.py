638a830209dac19ef4836210cbd874586e955060
Paul Reiter <reiter.paul@gmail.com>
2021-12-07 18:41:26 +0100

    Fix type hints

--- 07/main.py
+++ 07/main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List, Tuple, Callable
+from typing import Callable, List, Optional, Tuple
 
 
 def read_puzzle_input(filename: str) -> List[int]:
@@ -12,13 +12,14 @@ def calc_fuel_cost(crabs: List[int], position: int) -> int:
 
 
 def find_minimal_fuel_cost_and_position(
-    crabs: List[int], fuel_cost_function=None
+    crabs: List[int],
+    fuel_cost_function: Optional[Callable[[List[int], int], int]] = None,
 ) -> Tuple[int, int]:
 
     if fuel_cost_function is None:
         fuel_cost_function = calc_fuel_cost
-    minimum_position = min(crabs)
-    fuel_cost = fuel_cost_function(crabs, min(crabs))
+    minimum_position: int = min(crabs)
+    fuel_cost: int = fuel_cost_function(crabs, min(crabs))
     for position in range(min(crabs), max(crabs) + 1):
         if (new_fuel_cost := fuel_cost_function(crabs, position)) < fuel_cost:
             fuel_cost = new_fuel_cost
