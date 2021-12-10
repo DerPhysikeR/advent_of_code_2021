8e57c85734998355454967c37895d1465ad81fa8
Paul Reiter <reiter.paul@gmail.com>
2021-12-10 08:57:15 +0100

    Remove unnecessary dependency injection

--- 10/main.py
+++ 10/main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, List, Tuple, Optional
+from typing import List, Tuple, Optional
 from enum import Enum
 
 
@@ -12,9 +12,7 @@ class LineState(Enum):
 PAIRING = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
 
-def check_line(
-    line: str, score_fun: Callable[[LineState, List[str], Optional[str]], int]
-) -> int:
+def score_line(line: str) -> Tuple[LineState, int]:
     check_list = []
     for letter in line:
         if letter in "([{<":
@@ -22,35 +20,32 @@ def check_line(
         elif letter == PAIRING[check_list[-1]]:
             check_list.pop()
         else:
-            return score_fun(LineState.CORRUPT, check_list, letter)
+            return LineState.CORRUPT, score_corrupt_line(letter)
     if check_list:
-        return score_fun(LineState.INCOMPLETE, check_list)
-    return score_fun(LineState.VALID, check_list)
+        return LineState.INCOMPLETE, score_incomp_line(check_list)
+    return LineState.VALID, 0
 
 
-ILLEGAL_SCORE = {")": 3, "]": 57, "}": 1197, ">": 25137}
+CORRUPT_SCORE = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
 
-def corruptness_scoring_function(
-    line_state: LineState, _: List[str], letter: Optional[str] = ""
-) -> int:
-    if line_state == LineState.CORRUPT:
-        return ILLEGAL_SCORE[letter]
-    return 0
+def score_corrupt_line(letter: str) -> int:
+    return CORRUPT_SCORE[letter]
 
 
 def calc_corruptnes_score(lines: List[str]) -> int:
-    return sum(check_line(line, corruptness_scoring_function) for line in lines)
+    score = 0
+    for line in lines:
+        state, line_score = score_line(line)
+        if state == LineState.CORRUPT:
+            score += line_score
+    return score
 
 
 INCOMP_SCORE = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
-def incompletness_scoring_function(
-    line_state: LineState, remaining_parens: List[str], letter: Optional[str] = ""
-) -> int:
-    if line_state != LineState.INCOMPLETE:
-        return 0
+def score_incomp_line(remaining_parens: List[str]) -> int:
     score = 0
     for closing_paren in [PAIRING[p] for p in reversed(remaining_parens)]:
         score = 5 * score + INCOMP_SCORE[closing_paren]
@@ -58,11 +53,12 @@ def incompletness_scoring_function(
 
 
 def calc_incompletness_score(lines: List[str]) -> int:
-    all_incomp_scores = sorted(
-        [check_line(line, incompletness_scoring_function) for line in lines]
-    )
-    incomp_scores = [score for score in all_incomp_scores if score > 0]
-    return incomp_scores[(len(incomp_scores) - 1) // 2]
+    scores = []
+    for line in lines:
+        state, line_score = score_line(line)
+        if state == LineState.INCOMPLETE:
+            scores.append(line_score)
+    return sorted(scores)[(len(scores) - 1) // 2]
 
 
 def read_puzzle_input(filename: str) -> List[str]:
