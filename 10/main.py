bca1de308017508d48c4ec030978e65cfac4fb3a
Paul Reiter <reiter.paul@gmail.com>
2021-12-10 08:24:34 +0100

    Refactor solution for day 10 part 1 for part 2

--- 10/main.py
+++ 10/main.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
-from typing import List
+from typing import Callable, List, Tuple, Optional
 from enum import Enum
+
+
+class LineState(Enum):
+    VALID = "valid"
+    CORRUPT = "corrupt"
+    INCOMPLETE = "incomplete"
 
 
 PAIRING = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
-ILLEGAL_SCORE = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
-
-def check_line(line: str) -> int:
+def check_line(
+    line: str, score_fun: Callable[[LineState, List[str], Optional[str]], int]
+) -> int:
     check_list = []
     for letter in line:
         if letter in "([{<":
@@ -16,14 +22,25 @@ def check_line(line: str) -> int:
         elif letter == PAIRING[check_list[-1]]:
             check_list.pop()
         else:
-            return ILLEGAL_SCORE[letter]
+            return score_fun(LineState.CORRUPT, check_list, letter)
     if check_list:
-        return 0
+        return score_fun(LineState.INCOMPLETE, check_list)
+    return score_fun(LineState.VALID, check_list)
+
+
+ILLEGAL_SCORE = {")": 3, "]": 57, "}": 1197, ">": 25137}
+
+
+def corruptness_scoring_function(
+    line_state: LineState, _: List[str], letter: Optional[str] = ""
+) -> int:
+    if line_state == LineState.CORRUPT:
+        return ILLEGAL_SCORE[letter]
     return 0
 
 
 def calc_corruptnes_score(lines: List[str]) -> int:
-    return sum(check_line(line) for line in lines)
+    return sum(check_line(line, corruptness_scoring_function) for line in lines)
 
 
 def read_puzzle_input(filename: str) -> List[str]:
