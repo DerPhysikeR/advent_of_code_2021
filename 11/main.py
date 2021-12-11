aa30f07b58e8da86b151e7e4990690b49bdb831f
Paul Reiter <reiter.paul@gmail.com>
2021-12-11 10:06:06 +0100

    Fix new style type hints for Python >=3.9

--- 11/main.py
+++ 11/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Dict, List, Tuple, NamedTuple, Iterator, Set, ItemsView, Iterable
+from typing import NamedTuple
+from collections.abc import ItemsView, Iterable, Iterator
 from itertools import product
 
 
@@ -8,25 +9,25 @@ class Point(NamedTuple):
     col: int
 
     def neighbors(self) -> Iterator[Point]:
-        d: Tuple[int, int, int] = (-1, 0, 1)
+        d: tuple[int, int, int] = (-1, 0, 1)
         for dr, dc in product(d, d):
             if not dr == dc == 0:
                 yield Point(self.row + dr, self.col + dc)
 
 
 class Grid:
-    def __init__(self, energy_levels: List[List[int]]):
+    def __init__(self, energy_levels: list[list[int]]):
         self.width: int = len(energy_levels[0])
         self.height: int = len(energy_levels)
-        self.energy_levels: Dict[Point, int] = {}
+        self.energy_levels: dict[Point, int] = {}
         for ri, row in enumerate(energy_levels):
             for ci, el in enumerate(row):
                 self.energy_levels[Point(ri, ci)] = el
 
     def _evolve(self) -> int:
         self.increment_by_one()
-        flashes: Set[Point] = self.find_flashes()
-        new_flashes: Set[Point] = flashes.copy()
+        flashes: set[Point] = self.find_flashes()
+        new_flashes: set[Point] = flashes.copy()
         while new_flashes:
             new_flash_neighbors: Iterator[Point] = (
                 n for fl in new_flashes for n in fl.neighbors()
@@ -37,7 +38,7 @@ class Grid:
         self.nullify(flashes)
         return len(flashes)
 
-    def find_flashes(self) -> Set[Point]:
+    def find_flashes(self) -> set[Point]:
         return set([p for p, el in self.items() if el > 9])
 
     def increment_by_one(self, points: Iterable[Point] = None):
@@ -49,7 +50,7 @@ class Grid:
             except KeyError:
                 pass
 
-    def nullify(self, points: Set[Point]):
+    def nullify(self, points: set[Point]):
         for point in points:
             self[point] = 0
 
@@ -81,12 +82,12 @@ class Grid:
         return "\n".join(rows)
 
 
-def read_puzzle_input(filename: str) -> List[List[int]]:
+def read_puzzle_input(filename: str) -> list[list[int]]:
     with open(filename) as stream:
         return [[int(n) for n in row] for row in stream.read().strip().split("\n")]
 
 
 if __name__ == "__main__":
-    puzzle_input: List[List[int]] = read_puzzle_input("input.txt")
+    puzzle_input: list[list[int]] = read_puzzle_input("input.txt")
     print(Grid(puzzle_input).evolve(100))
     print(Grid(puzzle_input).find_first_sync_flash())
