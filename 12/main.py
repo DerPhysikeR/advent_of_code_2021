8448f1c65c1714793344c1c8da63eca6864a2e70
Paul Reiter <reiter.paul@gmail.com>
2021-12-12 10:29:22 +0100

    Add solution for day 12 part 2

--- 12/main.py
+++ 12/main.py
@@ -31,9 +31,39 @@ def find_paths(
             yield from find_paths(graph, n, visited)
 
 
+def twice_visited(path: tuple[str, ...], n: str) -> bool:
+    if n not in path:
+        return False
+    if n == "start":
+        return True
+    lower_nodes = [n for n in path if n.islower()]
+    set_lower_nodes = set(lower_nodes)
+    if len(set_lower_nodes) == len(lower_nodes):
+        return False
+    return True
+
+
+def find_paths_revisited(
+    graph: dict[str, set[str]], node: str = "start", visited: tuple[str, ...] = None
+):
+    visited = visited + (node,) if visited else (node,)
+    if node == "end":
+        yield visited
+    else:
+        for n in graph[node]:
+            if n.islower() and twice_visited(visited, n):
+                continue
+            yield from find_paths_revisited(graph, n, visited)
+
+
 def calc_n_paths(connections: list[Connection]):
     graph: dict[str, set[str]] = construct_graph(connections)
     return sum(1 for _ in find_paths(graph))
+
+
+def calc_n_paths_revisited(connections: list[Connection]):
+    graph: dict[str, set[str]] = construct_graph(connections)
+    return sum(1 for _ in find_paths_revisited(graph))
 
 
 def read_puzzle_input(filename: str) -> list[Connection]:
@@ -48,3 +78,4 @@ def read_puzzle_input(filename: str) -> list[Connection]:
 if __name__ == "__main__":
     puzzle_input: list[Connection] = read_puzzle_input("input.txt")
     print(calc_n_paths(puzzle_input))
+    print(calc_n_paths_revisited(puzzle_input))
--- 12/test_main.py
+++ 12/test_main.py
@@ -2,6 +2,7 @@ import pytest
 from main import (
     read_puzzle_input,
     calc_n_paths,
+    calc_n_paths_revisited,
 )
 
 
@@ -16,3 +17,16 @@ from main import (
 def test_calc_n_paths_with_test_inputs(test_input, n_paths):
     puzzle_input = read_puzzle_input(test_input)
     assert n_paths == calc_n_paths(puzzle_input)
+
+
+@pytest.mark.parametrize(
+    "test_input, n_paths",
+    [
+        ("small_test_input.txt", 36),
+        ("medium_test_input.txt", 103),
+        ("large_test_input.txt", 3509),
+    ],
+)
+def test_calc_n_paths_revisited_with_test_inputs(test_input, n_paths):
+    puzzle_input = read_puzzle_input(test_input)
+    assert n_paths == calc_n_paths_revisited(puzzle_input)
