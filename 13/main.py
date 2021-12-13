f6ec6a9889d0362ece07e7a0b74122128ccac5a5
Paul Reiter <reiter.paul@gmail.com>
2021-12-13 09:29:08 +0100

    Add solution for day 13 part 1

---
+++ 13/main.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+from typing import NamedTuple
+from parse import parse
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    def __getitem__(self, coord: str):
+        assert coord in "xy"
+        return self.x if coord == "x" else self.y
+
+    def change(self, coord: str, value: int):
+        assert coord in "xy"
+        if coord == "x":
+            return Point(value, self.y)
+        return Point(self.x, value)
+
+
+class Fold(NamedTuple):
+    coord: str
+    value: int
+
+
+class Paper:
+    def __init__(self, points: set[Point]):
+        self.points = points
+
+    def fold(self, fold: Fold):
+        points: set[Point] = set()
+        for point in self.points:
+            if point[fold.coord] < fold.value:
+                points.add(point)
+            else:
+                new_value = 2 * fold.value - point[fold.coord]
+                points.add(point.change(fold.coord, new_value))
+        return Paper(points)
+
+    def __len__(self):
+        return len(self.points)
+
+    def __repr__(self):
+        return f"Paper(points={self.points})"
+
+    def min_max(self) -> tuple[Point, Point]:
+        min_x: int = min(p.x for p in self.points)
+        min_y: int = min(p.y for p in self.points)
+        max_x: int = max(p.x for p in self.points)
+        max_y: int = max(p.y for p in self.points)
+        return Point(min_x, min_y), Point(max_x, max_y)
+
+    def __str__(self):
+        minp: Point
+        maxp: Point
+        minp, maxp = self.min_max()
+        rows: list[str] = []
+        for y in range(minp.y, maxp.y + 1):
+            rows.append("".join("#" if Point(x, y) in self.points else "." for x in
+                range(minp.x, maxp.x + 1)))
+        return "\n".join(rows)
+
+
+def read_puzzle_input(filename: str) -> tuple[set[Point], list[Fold]]:
+    with open(filename) as stream:
+        points = set()
+        for line in stream:
+            if line == "\n":
+                break
+            points.add(Point(*parse("{:d},{:d}", line.strip()).fixed))
+        folds = []
+        for line in stream:
+            folds.append(Fold(*parse("fold along {:l}={:d}", line.strip()).fixed))
+    return points, folds
+
+
+if __name__ == "__main__":
+    points: set[Point]
+    folds: list[Fold]
+    points, folds = read_puzzle_input("input.txt")
+    print(len(Paper(points).fold(folds[0])))
---
+++ 13/test_main.py
@@ -0,0 +1,29 @@
+import pytest
+from main import (
+    Point,
+    Fold,
+    Paper,
+    read_puzzle_input,
+)
+
+
+
+@pytest.fixture
+def puzzle_input():
+    return read_puzzle_input("test_input.txt")
+
+
+def test_read_puzzle_input(puzzle_input):
+    points, folds = puzzle_input
+    assert 18 == len(points)
+    assert Point(6, 10) in points
+    assert Point(9, 0) in points
+    assert [Fold("y", 7), Fold("x", 5)] == folds
+
+
+def test_paper_fold(puzzle_input):
+    points, folds = puzzle_input
+    paper = Paper(points)
+    assert 18 == len(paper)
+    assert 17 == len(paper := paper.fold(folds[0]))
+    assert 16 == len(paper := paper.fold(folds[1]))
