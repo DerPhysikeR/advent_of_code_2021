c157eb5845ee80fa6cd2105c24ae78c91b8d8081
Paul Reiter <reiter.paul@gmail.com>
2021-12-14 21:36:08 +0100

    Add all type hints

--- 14/main.py
+++ 14/main.py
@@ -1,23 +1,24 @@
 from __future__ import annotations
 from collections import defaultdict
 from itertools import pairwise
+from typing import Iterator
 
 
 class Polymer:
     def __init__(self, template: str, rules: dict[str, str]):
-        self.poly = list(template)
-        self.links = list(range(1, len(template))) + [0]
-        self.rules = rules
-        self.start = 0
+        self.poly: list[str] = list(template)
+        self.links: list[int] = list(range(1, len(template))) + [0]
+        self.rules: dict[str, str] = rules
+        self.start: int = 0
 
-    def evolve(self, n):
+    def evolve(self, n: int) -> Polymer:
         for _ in range(n):
             self._evolve()
         return self
 
     def _evolve(self):
-        idx1 = self.start
-        idx2 = self.links[idx1]
+        idx1: int = self.start
+        idx2: int = self.links[idx1]
         while True:
             if (key := "".join([self.poly[idx1], self.poly[idx2]])) in self.rules:
                 self.poly.append(self.rules[key])
@@ -27,32 +28,32 @@ class Polymer:
             if idx2 == self.start:
                 break
 
-    def mc_minus_lc_element_count(self):
-        element_counts = defaultdict(int)
+    def mc_minus_lc_element_count(self) -> int:
+        element_counts: dict[str, int] = defaultdict(int)
         for element in self.poly:
             element_counts[element] += 1
-        counts = sorted(list(element_counts.values()))
+        counts: list[int] = sorted(list(element_counts.values()))
         return counts[-1] - counts[0]
 
-    def __iter__(self):
-        idx = self.start
+    def __iter__(self) -> Iterator[str]:
+        idx: int = self.start
         while True:
             yield self.poly[idx]
             idx = self.links[idx]
             if idx == self.start:
                 break
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "".join(letter for letter in self)
 
 
 class FastPolymer:
     def __init__(self, template: str, rules: dict[str, str]):
-        self.rules = rules
-        self.element_counts = defaultdict(
+        self.rules: dict[str, str] = rules
+        self.element_counts: dict[str, int] = defaultdict(
             int, {k: template.count(k) for k in set(template)}
         )
-        self.pairing_counts = defaultdict(
+        self.pairing_counts: dict[str, int] = defaultdict(
             int,
             {
                 "".join(pair): template.count("".join(pair))
@@ -66,19 +67,18 @@ class FastPolymer:
         return self
 
     def _evolve(self):
-        new_pairing_counts = self.pairing_counts.copy()
+        new_pairing_counts: dict[str, int] = self.pairing_counts.copy()
         for key, insertion in self.rules.items():
             if key in self.pairing_counts:
-                count = self.pairing_counts[key]
+                count: int = self.pairing_counts[key]
                 self.element_counts[insertion] += count
                 new_pairing_counts[key] -= count
-                pair1, pair2 = key[0] + insertion, insertion + key[1]
-                new_pairing_counts[pair1] += count
-                new_pairing_counts[pair2] += count
+                new_pairing_counts[key[0] + insertion] += count
+                new_pairing_counts[insertion + key[1]] += count
         self.pairing_counts = new_pairing_counts
 
-    def mc_minus_lc_element_count(self):
-        counts = sorted(list(self.element_counts.values()))
+    def mc_minus_lc_element_count(self) -> int:
+        counts: list[int] = sorted(list(self.element_counts.values()))
         return counts[-1] - counts[0]
 
 
@@ -91,8 +91,8 @@ def read_puzzle_input(filename: str) -> tuple[str, dict[str, str]]:
 
 
 if __name__ == "__main__":
-    polymer = Polymer(*read_puzzle_input("input.txt"))
+    polymer: Polymer = Polymer(*read_puzzle_input("input.txt"))
     print(polymer.evolve(10).mc_minus_lc_element_count())
 
-    fast_polymer = FastPolymer(*read_puzzle_input("input.txt"))
+    fast_polymer: FastPolymer = FastPolymer(*read_puzzle_input("input.txt"))
     print(fast_polymer.evolve(40).mc_minus_lc_element_count())
