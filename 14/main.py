d3fb06c631a75bd9b36a4a35c81b62e554726d50
Paul Reiter <reiter.paul@gmail.com>
2021-12-14 21:29:42 +0100

    Add solution for day 14 part 2

--- 14/main.py
+++ 14/main.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-from reprlib import repr
-from typing import Iterable, NamedTuple
-from parse import parse
 from collections import defaultdict
+from itertools import pairwise
 
 
 class Polymer:
@@ -48,6 +46,41 @@ class Polymer:
         return "".join(letter for letter in self)
 
 
+class FastPolymer:
+    def __init__(self, template: str, rules: dict[str, str]):
+        self.rules = rules
+        self.element_counts = defaultdict(
+            int, {k: template.count(k) for k in set(template)}
+        )
+        self.pairing_counts = defaultdict(
+            int,
+            {
+                "".join(pair): template.count("".join(pair))
+                for pair in pairwise(template)
+            },
+        )
+
+    def evolve(self, n: int) -> FastPolymer:
+        for _ in range(n):
+            self._evolve()
+        return self
+
+    def _evolve(self):
+        new_pairing_counts = self.pairing_counts.copy()
+        for key, insertion in self.rules.items():
+            if key in self.pairing_counts:
+                count = self.pairing_counts[key]
+                self.element_counts[insertion] += count
+                new_pairing_counts[key] -= count
+                pair1, pair2 = key[0] + insertion, insertion + key[1]
+                new_pairing_counts[pair1] += count
+                new_pairing_counts[pair2] += count
+        self.pairing_counts = new_pairing_counts
+
+    def mc_minus_lc_element_count(self):
+        counts = sorted(list(self.element_counts.values()))
+        return counts[-1] - counts[0]
+
 
 def read_puzzle_input(filename: str) -> tuple[str, dict[str, str]]:
     with open(filename) as stream:
@@ -60,3 +93,6 @@ def read_puzzle_input(filename: str) -> tuple[str, dict[str, str]]:
 if __name__ == "__main__":
     polymer = Polymer(*read_puzzle_input("input.txt"))
     print(polymer.evolve(10).mc_minus_lc_element_count())
+
+    fast_polymer = FastPolymer(*read_puzzle_input("input.txt"))
+    print(fast_polymer.evolve(40).mc_minus_lc_element_count())
--- 14/test_main.py
+++ 14/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from main import (
     Polymer,
+    FastPolymer,
     read_puzzle_input,
 )
 
@@ -8,6 +9,11 @@ from main import (
 @pytest.fixture
 def polymer():
     return Polymer(*read_puzzle_input("test_input.txt"))
+
+
+@pytest.fixture
+def fast_polymer():
+    return FastPolymer(*read_puzzle_input("test_input.txt"))
 
 
 def test_polymer_evolve(polymer):
@@ -18,7 +24,32 @@ def test_polymer_evolve(polymer):
     assert "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB" == str(polymer.evolve(1))
 
 
-def test_mc_minus_lc_element_count(polymer):
-    polymer.evolve(10)
-    assert 1588 == polymer.mc_minus_lc_element_count()
+@pytest.mark.parametrize(
+    "count, steps",
+    [
+        (1, 0),
+        (1, 1),
+        (5, 2),
+        (7, 3),
+        (1588, 10),
+    ],
+)
+def test_polymer_mc_minus_lc_element_count(polymer, count, steps):
+    polymer.evolve(steps)
+    assert count == polymer.mc_minus_lc_element_count()
 
+
+@pytest.mark.parametrize(
+    "count, steps",
+    [
+        (1, 0),
+        (1, 1),
+        (5, 2),
+        (7, 3),
+        (1588, 10),
+        (2188189693529, 40),
+    ],
+)
+def test_mc_minus_lc_element_count(fast_polymer, count, steps):
+    fast_polymer.evolve(steps)
+    assert count == fast_polymer.mc_minus_lc_element_count()
