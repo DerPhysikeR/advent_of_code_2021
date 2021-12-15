c86ef256efca7aba6c0c1467965a83b4e495155c
Paul Reiter <reiter.paul@gmail.com>
2021-12-15 21:54:16 +0100

    Remove unnecessary comments

--- 15/main.py
+++ 15/main.py
@@ -15,61 +15,6 @@ class Point(NamedTuple):
         yield Point(self.col, self.row - 1)
 
 
-# class Maze:
-#     def __init__(self, maze: list[list[int]]):
-#         self.maze: dict[Point, int] = {
-#             Point(ci, ri): risk
-#             for ri, row in enumerate(maze)
-#             for ci, risk in enumerate(row)
-#         }
-#         self.goal = Point(len(maze[0]) - 1, len(maze) - 1)
-#
-#     def neighbors(self, point: Point) -> Iterator[Point]:
-#         for p in point.neighbors():
-#             if p in self.maze:
-#                 yield p
-#
-#     def find_shortest_path(self) -> tuple[Point, ...]:
-#         return self.find_shortest_path_dijkstra()
-#
-#     def find_shortest_path_dijkstra(self) -> tuple[Point, ...]:
-#         looked_at_all_neighbors: set[Point] = set()
-#         previous: dict[Point, Point] = {}
-#         risk_to_point: dict[Point, int] = {Point(0, 0): 0}
-#         pq = PriorityQueue()
-#         pq.put((0, Point(0, 0)))
-#         while not pq.empty():
-#             risk, point = pq.get()
-#             looked_at_all_neighbors.add(point)
-#             for neighbor in self.neighbors(point):
-#                 if neighbor in looked_at_all_neighbors:
-#                     continue
-#                 cum_risk = risk + self.maze[neighbor]
-#                 if neighbor not in risk_to_point:
-#                     risk_to_point[neighbor] = cum_risk
-#                     previous[neighbor] = point
-#                     if neighbor == self.goal:
-#                         return self.reconstruct_path(self.goal, previous)
-#                 elif cum_risk < risk_to_point[neighbor]:
-#                     risk_to_point[neighbor] = cum_risk
-#                     previous[neighbor] = point
-#                 # pq.put((cum_risk - neighbor.col - neighbor.row, neighbor)) -> A*
-#                 pq.put((cum_risk, neighbor))
-#         return self.reconstruct_path(self.goal, previous)
-#
-#     def reconstruct_path(
-#         self, goal: Point, previous_dict: dict[Point, Point]
-#     ) -> tuple[Point, ...]:
-#         points = [goal]
-#         point = goal
-#         while point != Point(0, 0):
-#             points.append(point := previous_dict[point])
-#         return tuple(reversed(points))
-#
-#     def risk_of_path_without_start(self, path: tuple[Point, ...]) -> int:
-#         return sum(self.maze[p] for p in path[1:])
-
-
 def read_puzzle_input(filename: str) -> list[list[int]]:
     with open(filename) as stream:
         return [[int(r) for r in line] for line in stream.read().strip().split("\n")]
@@ -91,8 +36,6 @@ class CumSum:
             (
                 self.flr(row - 1, col),
                 self.flr(row, col - 1),
-                # find_lowest_risk(row + 1, col),
-                # find_lowest_risk(row, col + 1),
             )
         )
 
