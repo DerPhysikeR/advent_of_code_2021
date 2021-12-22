2a087e22d16ab6329fd1921f01344479a3864c4e
Paul Reiter <reiter.paul@gmail.com>
2021-12-22 23:59:30 +0100

    Add solution for day 22 part 2

--- 22/main.py
+++ 22/main.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
-from typing import Iterator, NamedTuple
+from typing import Iterable, Iterator, NamedTuple
 from parse import parse
+from math import prod
 
 
 class Cube(NamedTuple):
     x: int
     y: int
     z: int
+
+    def __sub__(self, other):
+        return Cube(self.x - other.x, self.y - other.y, self.z - other.z)
 
 
 class Cuboid(NamedTuple):
@@ -34,6 +38,12 @@ class Cuboid(NamedTuple):
         ymax: int = min(self.maxcube.y, other.maxcube.y)
         zmax: int = min(self.maxcube.z, other.maxcube.z)
         return Cuboid(Cube(xmin, ymin, zmin), Cube(xmax, ymax, zmax))
+
+    @property
+    def size(self):
+        if any(d + 1 <= 0 for d in self.maxcube - self.mincube):
+            return 0
+        return prod(ma - mi + 1 for ma, mi in zip(self.maxcube, self.mincube))
 
 
 class Instruction(NamedTuple):
@@ -77,10 +87,59 @@ def reboot(instructions: list[Instruction]) -> set[Cube]:
     return on_cubes
 
 
-def on_cubes_int_init_region(instructions: list[Instruction]) -> int:
+def on_cubes_in_init_region(instructions: list[Instruction]) -> int:
     return sum(1 if c in INIT_REGION else 0 for c in reboot(instructions))
+
+
+def count_cubes(shapes: list[Cuboid]):
+    sa = ShapeAdder()
+    for shape in shapes:
+        sa.add(shape)
+    return sa.calc_size()
+
+
+def count_after_instructions(instructions: list[Instruction]):
+    sa = ShapeAdder()
+    for switch, shape in instructions:
+        if switch:
+            sa.add(shape)
+        else:
+            sa.sub(shape)
+    return sa.calc_size()
+
+
+class ShapeAdder:
+    def __init__(self):
+        self.shapes: dict[tuple[int, ...], Cuboid] = {}
+        self.idx = 0
+
+    def add(self, cuboid):
+        new_elements = {}
+        for idx, shape in self.shapes.items():
+            intersection = cuboid.intersection(shape)
+            if intersection.size > 0:
+                new_elements[idx + (self.idx,)] = intersection
+        self.shapes[(self.idx,)] = cuboid
+        self.shapes = {**self.shapes, **new_elements}
+        self.idx += 1
+
+    def sub(self, cuboid):
+        new_elements = {}
+        for idx, shape in self.shapes.items():
+            intersection = cuboid.intersection(shape)
+            if intersection.size > 0:
+                new_elements[idx + (self.idx,)] = intersection
+        self.shapes = {**self.shapes, **new_elements}
+        self.idx += 1
+
+    def calc_size(self):
+        size = 0
+        for idx, shape in self.shapes.items():
+            size += shape.size * (-1) ** (len(idx) - 1)
+        return size
 
 
 if __name__ == "__main__":
     inp: list[Instruction] = read_puzzle_input("input.txt")
-    print(on_cubes_int_init_region(inp))
+    print(on_cubes_in_init_region(inp))
+    print(count_after_instructions(inp))
--- 22/test_main.py
+++ 22/test_main.py
@@ -6,18 +6,20 @@ from main import (
     read_puzzle_input,
     INIT_REGION,
     reboot,
-    on_cubes_int_init_region,
-    # Die,
-    # Player,
-    # Game,
-    # find_points_loser_times_dice_rolls,
-    # calc_new_position,
+    on_cubes_in_init_region,
+    count_cubes,
+    count_after_instructions,
 )
 
 
 @pytest.fixture
 def instructions():
     return read_puzzle_input("test_input.txt")
+
+
+@pytest.fixture
+def more_instructions():
+    return read_puzzle_input("large_test_input.txt")
 
 
 def test_read_puzzle_input(instructions):
@@ -34,4 +36,44 @@ def test_reboot(instructions):
 
 
 def test_on_cubes_int_init_region(instructions):
-    assert on_cubes_int_init_region(instructions) == 590784
+    assert on_cubes_in_init_region(instructions) == 590784
+
+
+@pytest.mark.parametrize(
+    "cuboid, size",
+    [
+        (Cuboid(Cube(0, 0, 0), Cube(0, 0, 0)), 1),
+        (Cuboid(Cube(0, 0, 0), Cube(1, 1, 1)), 8),
+        (Cuboid(Cube(0, 0, 0), Cube(2, 1, 0)), 6),
+    ],
+)
+def test_cuboid_size(cuboid, size):
+    assert cuboid.size == size
+
+
+def test_count_cubes_with_non_overlapping_cuboids():
+    cuboids = [
+        Cuboid(Cube(1, 1, 0), Cube(10, 10, 0)),
+        Cuboid(Cube(21, 21, 0), Cube(30, 30, 0)),
+    ]
+    assert count_cubes(cuboids) == 200
+
+
+def test_count_cubes_with_single_overlapping_cuboids():
+    cuboids = [
+        Cuboid(Cube(1, 0, 0), Cube(10, 0, 0)),
+        Cuboid(Cube(6, 0, 0), Cube(15, 0, 0)),
+    ]
+    assert count_cubes(cuboids) == 15
+
+
+def test_count_cubes_with_identical_cuboids():
+    cuboids = [
+        Cuboid(Cube(1, 0, 0), Cube(10, 0, 0)),
+        Cuboid(Cube(1, 0, 0), Cube(10, 0, 0)),
+    ]
+    assert count_cubes(cuboids) == 10
+
+
+def test_count_cubes_after_instructions(more_instructions):
+    assert count_after_instructions(more_instructions) == 2758514936282235
