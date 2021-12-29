51ff2a76e90c146e98970dd491feed7cc18e5b8d
Paul Reiter <reiter.paul@gmail.com>
2021-12-29 19:58:24 +0100

    Add solution for day 21 part 2

--- 21/main.py
+++ 21/main.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from types import GenericAlias
-from typing import Iterator, NamedTuple, TypeVar
-from enum import Enum
+from typing import NamedTuple
 from itertools import cycle, product
 from operator import attrgetter
+from collections import defaultdict
 
 
 class Die:
@@ -62,5 +61,72 @@ def find_points_loser_times_dice_rolls(starting_positions):
     return game.loser.points * game.die.n_rolls
 
 
+THROW_UNIVERSES = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+
+class DPlayer(NamedTuple):
+    position: int
+    points: int
+
+
+PDict = dict[DPlayer, int]
+
+
+def evolve(players: PDict):
+    new_players: defaultdict[DPlayer, int] = defaultdict(int)
+    for player, puni in players.items():
+        for throw, universes in THROW_UNIVERSES.items():
+            new_position = calc_new_position(player.position, throw)
+            new_points = player.points + new_position
+            new_player = DPlayer(new_position, new_points)
+            new_players[new_player] += puni * universes
+    return new_players
+
+
+def separate_by_points(players: PDict) -> tuple[PDict, PDict]:
+    players_not_won: PDict = {}
+    players_won: PDict = {}
+    for player, universes in players.items():
+        if player.points >= 21:
+            players_won[player] = universes
+        else:
+            players_not_won[player] = universes
+    return players_not_won, players_won
+
+
+def calc_new_position(position, throw):
+    return ((position - 1 + throw) % 10) + 1
+
+
+def find_winning_universe_counts(starting_positions):
+    player1 = DPlayer(starting_positions[0], 0)
+    players1 = {player1: 1}
+
+    player2 = DPlayer(starting_positions[1], 0)
+    players2 = {player2: 1}
+
+    p1_won_universes = 0
+    p2_won_universes = 0
+
+    p1turn = True
+    while players1 and players2:
+        if p1turn:
+            players1 = evolve(players1)
+            players1, won = separate_by_points(players1)
+            p1_won_universes += sum(
+                u1 * u2 for u1, u2 in product(won.values(), players2.values())
+            )
+            p1turn = False
+        else:
+            players2 = evolve(players2)
+            players2, won = separate_by_points(players2)
+            p2_won_universes += sum(
+                u1 * u2 for u1, u2 in product(players1.values(), won.values())
+            )
+            p1turn = True
+    return p1_won_universes, p2_won_universes
+
+
 if __name__ == "__main__":
     print(find_points_loser_times_dice_rolls((2, 8)))
+    print(find_winning_universe_counts((2, 8)))
--- 21/test_main.py
+++ 21/test_main.py
@@ -4,6 +4,8 @@ from main import (
     Player,
     Game,
     find_points_loser_times_dice_rolls,
+    calc_new_position,
+    find_winning_universe_counts,
 )
 
 
@@ -22,3 +24,22 @@ def test_game_play_until_one_player_reaches_1000():
 
 def test_find_points_loser_times_dice_rolls():
     assert 739785 == find_points_loser_times_dice_rolls((4, 8))
+
+
+@pytest.mark.parametrize(
+    "position, throw, new_position",
+    [
+        (9, 1, 10),
+        (8, 2, 10),
+        (10, 2, 2),
+        (9, 3, 2),
+    ],
+)
+def test_calc_new_position(position, throw, new_position):
+    assert new_position == calc_new_position(position, throw)
+
+
+def test_find_winning_universe_counts():
+    p1_win_count, p2_win_count = find_winning_universe_counts([4, 8])
+    assert p1_win_count == 444356092776315
+    assert p2_win_count == 341960390180808
