4bd8b2fc9a9891d66ba09f525f91d02aa64ded30
Paul Reiter <reiter.paul@gmail.com>
2022-01-02 22:42:41 +0100

    Add solution for day 4 part 1 in go

---
+++ 04/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type BingoBoard struct {
+	numbers [5][5]int
+}
+
+type BingoGame struct {
+	bingoBoard BingoBoard
+	hitNumbers []int
+}
+
+func (b *BingoGame) callNumber(number int) {
+	for _, row := range b.bingoBoard.numbers {
+		for _, n := range row {
+			if n == number {
+				b.hitNumbers = append(b.hitNumbers, n)
+				return
+			}
+		}
+	}
+}
+
+func (b *BingoGame) hasWon() bool {
+	if len(b.hitNumbers) < 5 {
+		return false
+	}
+
+	// check rows
+	for _, row := range b.bingoBoard.numbers {
+		count := 0
+		for _, n := range row {
+			for _, calledNumber := range b.hitNumbers {
+				if calledNumber == n {
+					count += 1
+				}
+			}
+		}
+		if count == 5 {
+			return true
+		}
+	}
+
+	// check cols
+	for colIdx := 0; colIdx < 5; colIdx++ {
+		count := 0
+		for _, row := range b.bingoBoard.numbers {
+			for _, calledNumber := range b.hitNumbers {
+				if calledNumber == row[colIdx] {
+					count += 1
+				}
+			}
+		}
+		if count == 5 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (b *BingoGame) score() int {
+	score := 0
+	for _, row := range b.bingoBoard.numbers {
+		for _, n := range row {
+			flag := true
+			for _, calledNumber := range b.hitNumbers {
+				if n == calledNumber {
+					flag = false
+					break
+				}
+			}
+			if flag {
+				score += n
+			}
+		}
+	}
+	return score
+}
+
+func findFirstWinningGameScore(bingoBoards *[]BingoBoard, numbersToCall *[]int) (int, error) {
+	bingoGames := []BingoGame{}
+	for _, bingoBoard := range *bingoBoards {
+		bingoGames = append(bingoGames, BingoGame{bingoBoard, []int{}})
+	}
+
+	for _, number := range *numbersToCall {
+		for idx := range bingoGames {
+			bingoGames[idx].callNumber(number)
+			if bingoGames[idx].hasWon() {
+				return bingoGames[idx].score() * number, nil
+			}
+		}
+	}
+	return 0, fmt.Errorf("No board won.")
+}
+
+func parseNumbers(numberString string) (*[]int, error) {
+	numbers := []int{}
+	for _, numberString := range strings.Split(numberString, ",") {
+		number, err := strconv.Atoi(numberString)
+		if err != nil {
+			return &[]int{}, fmt.Errorf("Could not convert to int: %w", err)
+		}
+		numbers = append(numbers, number)
+	}
+	return &numbers, nil
+}
+
+func parseBingoBoard(boardString string) (*BingoBoard, error) {
+	bingoBoard := [5][5]int{}
+	for rowIdx, row := range strings.Split(boardString, "\n") {
+		for colIdx, numberString := range strings.Fields(row) {
+			number, err := strconv.Atoi(numberString)
+			if err != nil {
+				return &BingoBoard{bingoBoard}, fmt.Errorf("Could not convert to int: %w", err)
+			}
+			bingoBoard[rowIdx][colIdx] = number
+		}
+	}
+	return &BingoBoard{bingoBoard}, nil
+}
+
+func readPuzzleInput(filePath string) (*[]int, *[]BingoBoard, error) {
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return &[]int{}, &[]BingoBoard{}, fmt.Errorf("File could not be read: %w", err)
+	}
+	fields := strings.Split(strings.TrimSpace(string(fileContent)), "\n\n")
+
+	numbers, err := parseNumbers(fields[0])
+	if err != nil {
+		return &[]int{}, &[]BingoBoard{}, fmt.Errorf("Numbers could not be parsed: %w", err)
+	}
+
+	bingoBoards := []BingoBoard{}
+	for _, field := range fields[1:] {
+		bingoBoard, err := parseBingoBoard(field)
+		if err != nil {
+			return &[]int{}, &[]BingoBoard{}, fmt.Errorf("BingoBoards could not be parsed: %w", err)
+		}
+		bingoBoards = append(bingoBoards, *bingoBoard)
+	}
+
+	return numbers, &bingoBoards, nil
+}
+
+func main() {
+	numbers, bingoBoards, err := readPuzzleInput("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	score, err := findFirstWinningGameScore(bingoBoards, numbers)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("The score of the first winning board is: %v\n", score)
+}
---
+++ 04/main_test.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFindFirstWinningGameScore(t *testing.T) {
+
+	t.Run("Calc result of part 1 with test input", func(t *testing.T) {
+		numbers, bingoBoards, err := readPuzzleInput("test_input.txt")
+		if err != nil {
+			t.Fatal("Test input could not be read.")
+		}
+		score, err := findFirstWinningGameScore(bingoBoards, numbers)
+		if err != nil {
+			t.Fatal("No winning board found")
+		}
+		want := 4512
+		if score != want {
+			t.Errorf("got %v want %v", score, want)
+		}
+	})
+
+}
+
+func TestBingoGame(t *testing.T) {
+
+	t.Run("Test callNumber", func(t *testing.T) {
+		bingoBoard := BingoBoard{[5][5]int{
+			{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+		}}
+		bingoGame := BingoGame{bingoBoard, []int{}}
+		bingoGame.callNumber(0)
+		bingoGame.callNumber(1)
+		if len(bingoGame.hitNumbers) != 1 || bingoGame.hitNumbers[0] != 1 {
+			t.Errorf("Expected %v, got %v", []int{1}, bingoGame.hitNumbers)
+		}
+	})
+
+	t.Run("Test if board wins by row", func(t *testing.T) {
+		bingoBoard := BingoBoard{[5][5]int{
+			{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+		}}
+		numbers := []int{1, 2, 3, 4, 5}
+		bingoGame := BingoGame{bingoBoard, numbers}
+		if !bingoGame.hasWon() {
+			t.Errorf("Winning BingoGame didn't win")
+		}
+	})
+
+	t.Run("Test if board wins by column", func(t *testing.T) {
+		bingoBoard := BingoBoard{[5][5]int{
+			{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+		}}
+		numbers := []int{1, 6, 11, 16, 21}
+		bingoGame := BingoGame{bingoBoard, numbers}
+		if !bingoGame.hasWon() {
+			t.Errorf("Winning BingoGame didn't win")
+		}
+	})
+
+	t.Run("Test board score", func(t *testing.T) {
+		bingoBoard := BingoBoard{[5][5]int{
+			{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+		}}
+		numbers := []int{1, 2, 3, 4, 5}
+		bingoGame := BingoGame{bingoBoard, numbers}
+		got := bingoGame.score()
+		want := 25*13 - 15
+		if want != got {
+			t.Errorf("Expected %v, got %v", want, got)
+		}
+	})
+
+}
