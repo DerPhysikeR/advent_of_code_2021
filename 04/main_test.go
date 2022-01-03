655fd65a7802b260ef02a6974def0c7e5850e763
Paul Reiter <reiter.paul@gmail.com>
2022-01-03 17:28:46 +0100

    Format code with go fmt

--- 04/main_test.go
+++ 04/main_test.go
@@ -39,13 +39,13 @@ func TestFindFirstWinningGameScore(t *testing.T) {
 }
 
 func TestBingoGame(t *testing.T) {
-    var bingoBoard = BingoBoard{[5][5]int{
-			{1, 2, 3, 4, 5},
-			{6, 7, 8, 9, 10},
-			{11, 12, 13, 14, 15},
-			{16, 17, 18, 19, 20},
-			{21, 22, 23, 24, 25},
-		}}
+	var bingoBoard = BingoBoard{[5][5]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25},
+	}}
 
 	t.Run("Test callNumber", func(t *testing.T) {
 		bingoGame := BingoGame{bingoBoard, []int{}}
