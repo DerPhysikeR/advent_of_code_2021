caffc6119734b5027c9d21149460dedf13cc39ee
Paul Reiter <reiter.paul@gmail.com>
2022-02-12 17:24:07 +0100

    Add solution for day 2 in Rust

---
+++ 02/rust/src/main.rs
@@ -0,0 +1,97 @@
+use std::fmt;
+use std::fs;
+
+enum Direction {
+    Up,
+    Down,
+    Forward,
+}
+
+struct Command {
+    direction: Direction,
+    units: u32,
+}
+
+#[derive(Clone, Copy)]
+struct Vector {
+    distance: u32,
+    depth: u32,
+}
+
+impl fmt::Display for Vector {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Vector(distance={}, depth={})",
+            self.distance, self.depth
+        )
+    }
+}
+
+fn parse_command(line: &str) -> Command {
+    let parts: Vec<&str> = line.split(" ").collect();
+    assert!(parts.len() == 2);
+    let direction = match parts[0] {
+        "up" => Direction::Up,
+        "down" => Direction::Down,
+        "forward" => Direction::Forward,
+        _ => panic!("Can't parse {}", parts[0]),
+    };
+    let units: u32 = parts[1].parse().unwrap();
+    Command { direction, units }
+}
+
+fn read_puzzle_input(filename: &str) -> Vec<Command> {
+    let content = fs::read_to_string(filename).expect("Couldn't read input file");
+    content.lines().map(|x| parse_command(x)).collect()
+}
+
+fn follow_course(course: &Vec<Command>) -> Vector {
+    let mut location = Vector {
+        distance: 0,
+        depth: 0,
+    };
+    for command in course {
+        match command.direction {
+            Direction::Up => location.depth -= command.units,
+            Direction::Down => location.depth += command.units,
+            Direction::Forward => location.distance += command.units,
+        }
+    }
+    location
+}
+
+fn follow_course_correctly(course: &Vec<Command>) -> Vector {
+    let mut location = Vector {
+        distance: 0,
+        depth: 0,
+    };
+    let mut aim = 0;
+    for command in course {
+        match command.direction {
+            Direction::Up => aim -= command.units,
+            Direction::Down => aim += command.units,
+            Direction::Forward => {
+                location.distance += command.units;
+                location.depth += aim * command.units;
+            }
+        }
+    }
+    location
+}
+
+fn main() {
+    let puzzle_input = read_puzzle_input("../input.txt");
+    let destination = follow_course(&puzzle_input);
+    println!(
+        "{} => {}",
+        destination,
+        destination.distance * destination.depth
+    );
+    let destination = follow_course_correctly(&puzzle_input);
+    println!(
+        "{} => {}",
+        destination,
+        destination.distance * destination.depth
+    );
+}
