bb026de1f41244795790bc1207e387832dceb73c
Paul Reiter <reiter.paul@gmail.com>
2022-02-12 15:29:10 +0100

    Refactor rust solution for day 1

--- 01/rust/src/main.rs
+++ 01/rust/src/main.rs
@@ -2,30 +2,25 @@ use std::fs;
 
 fn read_puzzle_input(filename: &str) -> Vec<i32> {
     let content = fs::read_to_string(filename).expect("Couldn't read file!");
-    let mut depths = Vec::new();
-    for line in content.lines() {
-        depths.push(line.parse().unwrap());
-    }
-    depths
+    content.lines().map(|x| x.parse().unwrap()).collect()
 }
 
 fn count_depth_increases(depths: &Vec<i32>) -> i32 {
     assert!(depths.len() >= 1);
     let mut count_increases = 0;
-    let mut previous_depth = depths[0];
-    for depth in depths {
-        if depth > &previous_depth {
+    for i in 1..depths.len() {
+        if depths[i] > depths[i - 1] {
             count_increases += 1;
         }
-        previous_depth = *depth;
     }
     count_increases
 }
 
 fn sum_sliding_window(depths: &Vec<i32>) -> Vec<i32> {
+    assert!(depths.len() >= 3);
     let mut sliding_window_sums = Vec::new();
     for i in 0..(depths.len() - 2) {
-        sliding_window_sums.push(depths[i] + depths[i + 1] + depths[i + 2]);
+        sliding_window_sums.push(depths[i..i + 3].iter().sum());
     }
     sliding_window_sums
 }
