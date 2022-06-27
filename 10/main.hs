8789562af80c5fc05a1eeb460fe0eb3a27d1152d
Paul Reiter <reiter.paul@gmail.com>
2022-06-27 18:36:12 +0200

    Refactor solution for day 10 in haskell

--- 10/main.hs
+++ 10/main.hs
@@ -1,7 +1,8 @@
+import Data.Either (lefts, rights)
 import Data.List (sort)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromJust, mapMaybe)
+import Data.Maybe (fromJust)
 
 getCompletion :: [Char] -> [Char] -> Either Char [Char]
 getCompletion stack [] = Right stack
@@ -12,15 +13,13 @@ getCompletion stack (x:xs)
     where charMap = M.fromList [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
           newX = fromJust (M.lookup x charMap)
 
-getCorruptionScore :: Either Char [Char] -> Int
-getCorruptionScore (Left x) = fromJust (M.lookup x scoreMap)
+getCorruptionScore :: Char -> Int
+getCorruptionScore x = fromJust (M.lookup x scoreMap)
     where scoreMap = M.fromList [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
-getCorruptionScore _ = 0
 
-getCompletionScore :: Either Char [Char] -> Maybe Int
-getCompletionScore (Right xs) = Just (foldl (\acc x -> fromJust (M.lookup x scoreMap) + (5 * acc)) 0 xs)
+getCompletionScore :: [Char] -> Int
+getCompletionScore = foldl (\acc x -> fromJust (M.lookup x scoreMap) + (5 * acc)) 0
     where scoreMap = M.fromList [(')', 1), (']', 2), ('}', 3), ('>', 4)]
-getCompletionScore _ = Nothing
 
 getOddMedian :: [Int] -> Int
 getOddMedian xs = sort xs !! m
@@ -33,5 +32,5 @@ main :: IO ()
 main = do
     lin <- readInput "input.txt"
     let completions = map (getCompletion []) lin
-    print (sum $ map getCorruptionScore completions)
-    print (getOddMedian $ mapMaybe getCompletionScore completions)
+    print (sum . map getCorruptionScore . lefts $ completions)
+    print (getOddMedian . map getCompletionScore . rights $ completions)
